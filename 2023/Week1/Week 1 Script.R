3edc3e1752e3b29eec46cc3c54c21496090fae8e
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2023-02-17 15:18:49 +0800

    Week 1 Update

---
+++ 2023/Week1/Week 1 Script.R
@@ -0,0 +1,44 @@
+1 + 2
+
+(235678 / 1729 + 25)^2 * 1000 + 50 * 1.5-29 / 77^3 + 99 * 20* 65 / 4-1 + 1550
+
+# There are two lines below, but it is one statement
+(235678 / 1729 + 25)^2 * 1000 + 50 * 
+  1.5-29 / 77^3 + 99 * 20* 65 / 4-1 + 1550
+
+# This is a comment
+
+# Variable
+num = 1 + 3
+
+num = 1 - 3
+
+num <- 1 + 3
+
+# Vector
+nums = 6:15
+
+nums[1]
+nums[7]
+
+nums[2] = 3
+
+# Strings
+str = "This is a string"
+
+resourcesURL = "https://lngproc.hss.nthu.edu.tw/statisticsR/Week1/"
+
+df = read.csv(paste(resourcesURL, "dummyDataFrame.csv", sep = ""))
+
+df[1]
+
+df["Age"]
+
+df$Age
+
+df[1, 2]
+
+df[1, ]
+
+df[1, "Age"]
+df[1, ]$Age
