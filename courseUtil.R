decb1edf93ed5c4cf768159b4b865e53d7d50d00
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2023-03-10 21:01:18 +0800

    Week 4 Update

--- courseUtil.R
+++ courseUtil.R
@@ -1,5 +1,5 @@
 loadCourseCSV = function(week = NULL, file = NULL) {
-  path = "https://lngproc.fl.nthu.edu.tw/statisticsR/"
+  path = "https://lngproc.hss.nthu.edu.tw/statisticsR/"
   return(read.csv(paste(path, week, "/", file, sep = ""), 
                     fileEncoding = "UTF-8-BOM", na.strings = c("NA", "na")))
 }
