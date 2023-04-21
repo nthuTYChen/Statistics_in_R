61780548bf35bf5a3bbd4daa8fefc1f38554fa62
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2023-04-21 22:31:14 +0800

    Update Week 7-9 Script.R

--- 2023/Week 7-9/Week 7-9 Script.R
+++ 2023/Week 7-9/Week 7-9 Script.R
@@ -132,57 +132,143 @@ t.test(x = group2, mu = 300)
 # due to randomness. That is, the difference is
 # statistically significant.
 
+# Load the library for the "durationsOnt" dataset, which is used to explore
+# the correlation between (log-transformed) word frequency and the duration
+# of the ont- prefix (in seconds)
 library(languageR)
+
+# Get Pearson's correlation coefficient for word frequency and prefix duration
+# r = -0.049; a very weak negative correlation.
 cor(durationsOnt$Frequency, 
     durationsOnt$DurationOfPrefix)
 
+# Explore if the very small negative r is significantly different from zero -
+# a question that can be answered in a Pearson's correlation test
+
+# Try to manually calculate the statistics based on the formula in the Week 7-9
+# handout.
+
+# Starting with covariance.
+
+# Get the mean for each of the two correlated variables.
 freq.mean = mean(durationsOnt$Frequency)
 dur.mean = mean(durationsOnt$DurationOfPrefix)
 
-sum.prod = sum((durationsOnt$Frequency - freq.mean) *
-        (durationsOnt$DurationOfPrefix - dur.mean))
+# Get the differences between every individual data point and the mean for
+# the two correlated variables
+freq.diffs = durationsOnt$Frequency - freq.mean
+dur.diffs = durationsOnt$DurationOfPrefix - dur.mean
 
-freq.dur.cov = sum.prod / (nrow(durationsOnt) - 1)
+# Get the "sum of products"
+sum.prod = sum(freq.diffs * dur.diffs)
 
+# Get the covariance of the two variables by dividing the sum of products
+# by the number of observations (i.e., the number of words) in durationsOnt.
+freq.dur.cov = sum.prod / (nrow(durationsOnt) - 1) # -0.003129659
+
+# You can also get the same covariance of two correlated variables using cov()
 cov(durationsOnt$Frequency, 
     durationsOnt$DurationOfPrefix)
 
+# A potential problem with the manual calculation is that a covariance might
+# be different depending on the scale of a variable. See the Week 7-9 handout
+# for a more detailed explanation.
+
+# Multiply word frequencies by 10 and store it into a variable called Freq.10x
+# in the dataset.
 durationsOnt$Freq.10x = durationsOnt$Frequency * 10
 
+# Try to get the covariance between Freq.10x and DurationOfPrefix.
+# The number is -0.03129659; it's also 10 times larger!
 cov(durationsOnt$Freq.10x, 
     durationsOnt$DurationOfPrefix)
 
+# The solution is to convert raw numbers into z-scores (i.e., SDs in a 
+# distribution where the original mean is converted into 0), so the difference 
+# in scale doesn't matter.
+
+# The difference between 5 and 10 is 5, and the z-scores are -0.7071068
+# and 0.7071068
 scale(c(5, 10))
+# The difference between 50 and 100 is 50 (ten times larger), but the z-scores
+# are still -0.7071068 and 0.7071068, because the distance between the two
+# values and the mean remains identical when the distance is converted into
+# z-scores.
 scale(c(50, 100))
 
-cov(scale(durationsOnt$Freq.10x), 
+# Thus, Pearson's correlation coefficient (r) is in fact a "standardized 
+# coefficient" which is calculated by z-scoring the correlated variables
+freq.dur.r = cov(scale(durationsOnt$Freq.10x), 
     scale(durationsOnt$DurationOfPrefix))
+# -0.04927405; same as the one we've got from the statement below, in which
+# the variables are NOT z-scored beforehand
+# cor(durationsOnt$Frequency, durationsOnt$DurationOfPrefix)
 
-freq.dur.cor.2 = cor(durationsOnt$Frequency, 
-    durationsOnt$DurationOfPrefix) ^ 2
+# Moving on to calculating the t statistics of the Pearson's correlation
+# coefficient.
 
-r = cor(durationsOnt$Frequency, 
-        durationsOnt$DurationOfPrefix)
+# Get r-squared first, which means the proportion of variance explained
+# by the correlation; # 0.0024 = 0.2%, a very small proportion.
+freq.dur.r2 = freq.dur.r ^ 2
+
+# Get the t-value based on all the information we have following the formula
+# in the Week 7-9 handout.
 n = nrow(durationsOnt)
-t = r / (sqrt(1 - freq.dur.cor.2) / sqrt(n - 2))
+t = freq.dur.r / (sqrt(1 - freq.dur.r2) / sqrt(n - 2)) # -0.493
 
+# Because t is negative, get one-tailed p-value from the lower tail of a 
+# t distribution for the probability of observing the negative correlation
+# and beyond. p = .311
 p = pt(t, df = n - 2, lower.tail = TRUE)
+# Get a two-tailed p-value; p = .623; the chance of observing the weak 
+# correlation due to total randomness is 62.3%, so the weak correlation is not
+# significantly different from 0 (i.e., no correlation).
 p * 2
 
-cor.test(x = scale(durationsOnt$Freq.10x), 
-         y = scale(durationsOnt$DurationOfPrefix))
+# You will get the same t statistics with cor.test(). Note that as in cor(),
+# the correlated variations do not have be z-scored first; the conversion is
+# done automatically.
+cor.test(x = durationsOnt$Frequency, 
+         y = durationsOnt$DurationOfPrefix)
+
+# Check Week 4-5 handout for the quick explanation of the experiment in
+# Myers (2015). In this part, we will explore if the judgment of 3000+ 
+# nonwords eventually became very tedious for the participants, so 
+# reaction times had become much faster or slower in the second half of the
+# experiment (the participants probably just wanted to end the experiment
+# as quickly as possible or just got tired and responded more slowly)
 
 source("https://lngproc.hss.nthu.edu.tw/statisticsR/courseUtil.R")
 Myers.clean = loadCourseCSV("Week7-9", "MyersClean.csv")
 
+# We have different number of data points in both sessions, so unpaired two
+# sample t-test is the only choice.
 table(Myers.clean$Session)
 
+# Get RTs from each session as two separate vectors.
 s1.rt = Myers.clean[Myers.clean$Session == 1,]$logRT
 s2.rt = Myers.clean[Myers.clean$Session == 2,]$logRT
 
+# Run an unpaired two-sample t-test assuming equal variance for both samples
+# (i.e., var = TRUE). We get two means 6.981007 and 7.038837, and the t 
+# statistics suggests that with a very small p-value (< .001), the 
+# distance between the two means, which is not zero, is very unlikely to
+# be observed due to total randomness. Thus, the distance is significantly
+# different from zero, and the two means are significantly different from
+# each other.
 t.test(x = s1.rt, y = s2.rt, var = TRUE)
 
+# However, do the two samples really have an equal variance? This question
+# could be answered using the var.test() function (i.e., variance test).
+# The results are stored to var.test.res
 var.test.res = var.test(x = s1.rt, y = s2.rt)
+# We only care about the p-value for now, which is also very small. That is,
+# the variances in the two samples are significantly different!
 var.test.res$p.value
 
+# Re-run the t-test by leaving out "var=TRUE" (it is by default FALSE).
+# We still get a p-value suggesting a significant difference between the two
+# sample means. But check the Week 7-9 handout for the explanations of the
+# differences between the outputs of the two statistical analyses as well as
+# their connection to Type I and Type II Errors.
 t.test(x = s1.rt, y = s2.rt)
