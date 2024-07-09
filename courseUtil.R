a0252f712d8109f0021b5cafd8ff5759c81022da
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2024-07-09 10:49:21 +0800

    2024 Update

--- courseUtil.R
+++ courseUtil.R
@@ -1,5 +1,5 @@
-loadCourseCSV = function(week = NULL, file = NULL) {
-  path = "https://lngproc.hss.nthu.edu.tw/statisticsR/"
-  return(read.csv(paste(path, week, "/", file, sep = ""), 
+loadCourseCSV = function(year = NULL, week = NULL, file = NULL) {
+  path = "https://raw.githubusercontent.com/nthuTYChen/Statistics_in_R/main/"
+  return(read.csv(paste(path, year, "/", week, "/", file, sep = ""), 
                     fileEncoding = "UTF-8-BOM", na.strings = c("NA", "na")))
 }
