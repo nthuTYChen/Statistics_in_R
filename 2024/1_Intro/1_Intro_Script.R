1e8b4e6fd74c9358478ff6af42e51325ed792ad2
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2024-09-06 13:01:57 +0800

    Week 1 Update

--- 2024/1_Intro/1_Intro_Script.R
+++ 2024/1_Intro/1_Intro_Script.R
@@ -10,28 +10,32 @@ setwd("~/GitHub/Statistics_in_R/2024/1_Intro")
   29 / 77 ^ 3 + 99 * 20 * 65 / 4 - 1 + 1550
 
 # Variable
-num = 1
-num2 <- 2
+num = 1   # The value on the right is assigned to the variable on the left.
+num2 <- 2 # This also works, and <- may be more straightforward to some.
 
-num
+num       # Retrieve the data assigned to 'num'
 
-#Num # Case-sensitive: It is different from 'num'
+#Num      # Case-sensitive: It is different from 'num', so you see an error.
 
-num = 1 + 3
+num = 1 + 3  # You can always change the value assigned to a variable.
 
 # Vector
-nums = 6:15
-manyNums = 10:90
+nums = 6:15       # 6:15 creates a number sequence 6, 7, 8, 9...15
+manyNums = 10:90  # 10:90 creates a number sequence 10, 11, 12, 13...90
 
-nums[3]
-nums[3:5]
-nums[c(3, 5, 7)]
+nums[3]           # Get the value from the third position of the nums vector
+nums[3:5]         # Get the value from the third to fifth positions of the nums vector
+# Get the third, fifth, and seventh value from the nums vector
+# c() 'combines' multiple values as a vector
+nums[c(3, 5, 7)]  
+# Get the values from 3rd-5th, 10th-12th, and 20th-22nd position of the manyNums vector
 manyNums[c(3:5, 10:12, 20:22)]
 
 # String
-string = "This is a string!"
-string
+string = "This is a string!"  # Strings/characters are enclosed in a pair of quotation marks
+string                        # Retrieve the string assigned to the variable
 
+# Create a string vector storing two strings combined using c()
 strings = c("This is the first string!", "This is the second string!")
-strings[1]
-strings[2]
+strings[1] # Retrieve the first string
+strings[2] # Retrieve the second string
