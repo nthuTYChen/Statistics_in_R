84267782bee27ef4f0783e43df7f423ab68ceb32
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2024-11-29 11:54:56 +0800

    Update 6_Regression.R

--- 2024/6_Regression/6_Regression.R
+++ 2024/6_Regression/6_Regression.R
@@ -36,3 +36,68 @@ log.freq.seq = 1:5
 # Calculate the predicted duration of the prefix nasal with the number sequence.
 # Now you can see where the regression line in Figure 1 comes from.
 0.055275 + -0.002052 * log.freq.seq
+
+dur.lm.int = lm(DurationPrefixNasal ~ 1, data = durationsOnt)
+summary(dur.lm.int)
+
+mean(durationsOnt$DurationPrefixNasal)
+
+model.res = dur.lm$residuals
+model.sse = sum(model.res ^ 2)
+model.sse
+
+null.res = dur.lm.int$residuals
+null.sse = sum(null.res ^ 2)
+null.sse
+
+1 - model.sse / null.sse
+
+dur.res = as.vector(dur.lm$residuals)
+qqnorm(dur.res, main = "Q-Q Plot of the residuals of dur.lm")
+qqline(dur.res, col = "red", lwd = 2)
+
+(-0.002052 - 0) / 0.001298
+
+pt(q = -1.581, df = 100) * 2
+
+nas.dur = durationsOnt$DurationPrefixNasal
+durationsOnt$DurPrefixN.z = (nas.dur - mean(nas.dur)) / sd(nas.dur)
+freq = durationsOnt$Frequency
+durationsOnt$Freq.z = (freq - mean(freq)) / sd(freq)
+
+dur.lm.z = lm(formula = DurPrefixN.z ~ Freq.z, data = durationsOnt)
+summary(dur.lm.z)
+
+sr = durationsOnt$SpeechRate
+durationsOnt$sr.z = (sr - mean(sr)) / sd(sr)
+
+dur.sr.lm.z = lm(formula = DurPrefixN.z ~ sr.z, data = durationsOnt)
+summary(dur.sr.lm.z)
+
+table(durationsOnt$PlosivePresent)
+
+durationsOnt$Pl.Pr.Fac = as.factor(durationsOnt$PlosivePresent)
+contrasts(durationsOnt$Pl.Pr.Fac)
+
+dur.lm.plo = lm(DurationPrefixNasal ~ Pl.Pr.Fac, data = durationsOnt)
+summary(dur.lm.plo)
+
+mean(durationsOnt[durationsOnt$Pl.Pr.Fac == "no",]$DurationPrefixNasal)
+mean(durationsOnt[durationsOnt$Pl.Pr.Fac == "yes",]$DurationPrefixNasal)
+
+0.04285096 - 0.06822025
+
+t.test(formula = DurationPrefixNasal ~ PlosivePresent, var.equal = T,
+       data = durationsOnt)
+
+durationsOnt$Pl.Pr.Fac.Sum = as.factor(durationsOnt$PlosivePresent)
+contrasts(durationsOnt$Pl.Pr.Fac.Sum) = contr.sum(2)
+contrasts(durationsOnt$Pl.Pr.Fac.Sum)
+
+dur.lm.plo.sum = lm(formula = DurationPrefixNasal ~ Pl.Pr.Fac.Sum,
+                    data = durationsOnt)
+summary(dur.lm.plo.sum)
+
+dur.pl.aov = aov(formula = DurationPrefixNasal ~ PlosivePresent,
+                 data = durationsOnt)
+summary(dur.pl.aov)
