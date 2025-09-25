5e03add28f658517784de165923f31d47ae2228a
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2025-09-25 16:56:00 +0800

    Update courseUtil.R

--- courseUtil.R
+++ courseUtil.R
@@ -3,3 +3,15 @@ loadCourseCSV = function(year = NULL, topic = NULL, file = NULL) {
   return(read.csv(paste(path, year, "/", topic, "/", file, sep = ""), 
                     fileEncoding = "UTF-8-BOM", na.strings = c("NA", "na")))
 }
+
+compNormSamp = function(data = NULL) {
+  data.mean = mean(data)
+  data.sd = sd(data)
+  data.den = density(data)
+  plot(data.den, main = "Sample vs. Theoretical Distribution",
+       xlim = c(data.mean - data.sd * 3, data.mean + data.sd * 3))
+  data.seq = seq(from = data.mean - data.sd * 3, to = data.mean + data.sd * 3,
+                 by = .1)
+  data.norm.den = dnorm(data.seq, mean = data.mean, sd = data.sd)
+  lines(x = data.seq, y = data.norm.den, col = "purple", lty = 2, lwd = 2)
+}
